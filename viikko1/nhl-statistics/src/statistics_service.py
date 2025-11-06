8194e78f712daf95c90b3e784a180c024dd98b8c
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-06 14:01:24 +0200

    Vaihdoin statistics_service.py koodin oikeaksi

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -1,22 +1,22 @@
 from player_reader import PlayerReader
+from enum import Enum
 
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
-
-
-        self._players = reader.get_players()
-
+    def __init__(self, reader):
+        self.reader = reader
+        self._players = self.reader.get_players()
 
     def search(self, name):
         for player in self._players:
             if name in player.name:
                 return player
 
-
         return None
-
 
     def team(self, team_name):
         players_of_team = filter(
@@ -24,28 +24,17 @@ class StatisticsService:
             self._players
         )
 
-
         return list(players_of_team)
 
+    def top(self, how_many, sort_by=SortBy.POINTS):
+        if sort_by == SortBy.POINTS:
+            key_func = lambda player: player.points
+        elif sort_by == SortBy.GOALS:
+            key_func = lambda player: player.goals
+        elif sort_by == SortBy.ASSISTS:
+            key_func = lambda player: player.assists
+        else:
+            raise ValueError("Tuntematon lajitteluperuste")
 
-    def top(self, how_many):
-        # metodin käyttämä apufufunktio voidaan määritellä näin
-        def sort_by_points(player):
-            return player.points
-
-
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
-
-
-        result = []
-        i = 0
-        while i <= how_many:
-            result.append(sorted_players[i])
-            i += 1
-
-
-        return result
+        sorted_players = sorted(self._players, reverse=True, key=key_func)
+        return sorted_players[:how_many]
