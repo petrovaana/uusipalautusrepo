2bcbc2c5229a4c44c8eb291d5906cc98dd0a5a47
petrovaana <petrova.anastasiahusu@gmail.com>
2025-11-19 16:00:18 +0200

    Tehtävä 4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -11,47 +11,86 @@ from src.varasto import Varasto
 from src.tuote import Tuote
 
 class TestKauppa(unittest.TestCase):
-    def test_maksettaessa_ostos_pankin_metodia_tilisiirto_kutsutaan(self):
-        pankki_mock = Mock()
-        viitegeneraattori_mock = Mock()
-
+    def setUp(self):
+        self.pankki_mock = Mock()
+        self.viitegeneraattori_mock = Mock()
+        self.ostoskori_mock = Mock()
+        self.varasto_mock = Mock()
         # palautetaan aina arvo 42
-        viitegeneraattori_mock.uusi.return_value = 42
+        self.viitegeneraattori_mock.uusi.return_value = 42
 
-        varasto_mock = Mock()
+        self.tuote1 = Tuote(1, "maito", 5)
+        self.tuote2 = Tuote(2, "vesi", 3)
+        self.tuote3 = Tuote(3, "loppu", 2)
 
-        # tehdään toteutus saldo-metodille
         def varasto_saldo(tuote_id):
             if tuote_id == 1:
                 return 10
             if tuote_id == 2:
+                return 10
+            if tuote_id == 3:
                 return 0
+            
 
-        # tehdään toteutus hae_tuote-metodille
         def varasto_hae_tuote(tuote_id):
             if tuote_id == 1:
-                return Tuote(1, "maito", 5)
+                return self.tuote1
             if tuote_id == 2:
-                return Tuote(2, "vesi", 3)
+                return self.tuote2
+            if tuote_id == 3:
+                return self.tuote3
+            
 
-        # otetaan toteutukset käyttöön
-        varasto_mock.saldo.side_effect = varasto_saldo
-        varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
 
         # alustetaan kauppa
-        kauppa = Kauppa(varasto_mock, pankki_mock, viitegeneraattori_mock)
-
-        # tehdään ostokset
-        kauppa.aloita_asiointi()
-        kauppa.lisaa_koriin(1)
-        kauppa.lisaa_koriin(2)
-        kauppa.tilimaksu("pekka", "12345")
+        self.kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)    
+                    
+    def test_maksettaessa_ostos_pankin_metodia_tilisiirto_kutsutaan(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
 
         # varmistetaan, että metodia tilisiirto on kutsuttu
-        pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", ANY, 5)
-        # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", ANY, 5)
+        # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista       
 
-    #Testasin vaa muuttamalla annettuja arvoja. Vaihoin esim toho varasto_hae_tuote
-    #toisen tuotteen ja sit varasto_saldoon et onks toi toinen loppu vai ei eli salo 0
-    #vai enemmän ja sit noit ku tehää ostoksii niin muunsin ain et lisättiiks koriin 
-    #tuote 1 vai tuote 2
+    def test_kaksi_tuotetta_oikea_summa(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+
+        self.kauppa.tilimaksu("minttu", "100")
+
+        self.pankki_mock.tilisiirto.assert_called_with("minttu", 42, "100", ANY, 5+3)
+
+    def test_loppunut_tuote(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.tilimaksu("loppu", "111")
+
+        self.pankki_mock.tilisiirto.assert_called_with("loppu", 42, "111", ANY, 5)
+    
+    def test_aloitus(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+
+        self.kauppa.tilimaksu("nollaus", "112")
+
+        self.pankki_mock.tilisiirto.assert_called_with("nollaus", 42, "112", ANY, 3)
+    
+    def test_poista_korista(self):
+        ostoskori_mock = Mock()
+        self.kauppa._ostoskori = ostoskori_mock
+
+        self.kauppa.poista_korista(1)
+
+        self.varasto_mock.hae_tuote.assert_called_with(1)
+        ostoskori_mock.poista.assert_called_with(self.tuote1)
+        self.varasto_mock.palauta_varastoon.assert_called_with(self.tuote1)
+    #Testattavuus nyt 87% 
